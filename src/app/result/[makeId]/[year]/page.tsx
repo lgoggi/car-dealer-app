0496f8b8a53be90d435293fbfc2e9527e5dfa926
Lucas Goggi <lucas.agoggi@gmail.com>
2025-01-20 19:41:40 -0300

    add generateStaticParams

--- src/app/result/[makeId]/[year]/page.tsx
+++ src/app/result/[makeId]/[year]/page.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 import { CarsModels } from "@/components/CarsModels";
+import { ICar } from "@/types/types";
 
-export default async function Result({ params }: { params: { makeId: string, year: string }}) {
+type Params = Promise<{ makeId: string, year: string }>
+
+export async function generateStaticParams() {
+  const cars = await fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json').then((res) => res.json())
+  const getYearList = () => {
+    const currentYear = new Date().getFullYear();
+    const resultArr = [];
+    let year = 2015;
+    while (year <= currentYear) {
+        resultArr.push(year);
+        ++year;
+    }
+    return resultArr;
+  }
+  const yearList = getYearList();
+  return cars.Results.map((car: ICar) => ({
+    makeId: car.MakeId.toString(),
+    year: yearList.toString()
+  }))
+}
+
+export default async function Result({ params }: { params: Params}) {
   const {makeId, year} = await params;
   const response = await fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`);
   const carsModels = (await response.json()).Results;
